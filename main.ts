bd8a1fb09266535e492906f12685c107be280ef8
poedba <37512528+poedba@users.noreply.github.com>
2022-07-27 09:38:58 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
+function is_it_not_dark (bool: boolean, bool2: boolean) {
+	
+}
 hummingbird.startHummingbird()
 basic.forever(function () {
-    hummingbird.setLED(ThreePort.One, hummingbird.getSensor(SensorType.Light, ThreePort.One))
+    let is_it_dark = 0
+    if (is_it_dark) {
+        hummingbird.setLED(ThreePort.One, hummingbird.getSensor(SensorType.Light, ThreePort.One))
+    } else {
+        hummingbird.setLED(ThreePort.One, 0)
+    }
 })
